5b83122e2c28bc42d9112b97848747c356c6cde1
tiagopazhs <tiagopaz.hs@gmail.com>
2023-02-23 19:32:40 -0300

    remove warm

--- web/src/components/SubNavBar.js
+++ web/src/components/SubNavBar.js
@@ -1,4 +1,4 @@
-import { Box, Grid } from "@mui/material";
+import { Grid } from "@mui/material";
 
 export default function SubNavBar(props) {
     return (
@@ -9,10 +9,10 @@ export default function SubNavBar(props) {
             <Grid item xs={6}>
                 <Grid container className="m-0 p-0 mt-2 mb-3" spacing={0}>
                     <Grid item xs={12} md={6}>
-                        <body style={{ backgroundColor: "#F5F6FC", display: 'flex', justifyContent: "flex-end", alignItems: "center" }} className="text-muted pe-4">  Last update: {props.updatedTime}</body>
+                        <div style={{ backgroundColor: "#F5F6FC", display: 'flex', justifyContent: "flex-end", alignItems: "center" }} className="text-muted pe-4">Last update: {props.updatedTime}</div>
                     </Grid>
                     <Grid item xs={12} md={6}>
-                        <body style={{ backgroundColor: "#F5F6FC", display: 'flex', justifyContent: "flex-end", alignItems: "center" }} className="text-muted pe-4">Period: Current month</body>
+                        <div style={{ backgroundColor: "#F5F6FC", display: 'flex', justifyContent: "flex-end", alignItems: "center" }} className="text-muted pe-4">Period: Current month</div>
                     </Grid>
                 </Grid>
             </Grid>
